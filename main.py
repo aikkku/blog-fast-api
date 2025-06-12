56085192e6cd98bb040201fd647ed4b29c73f392
Asilbek Ismatilloev <asilbek@Asilbeks-MacBook-Air.local>
2025-06-12 16:56:57 -0400

    path parameters

--- main.py
+++ main.py
@@ -2,10 +2,22 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get('/')
 def index():
-    return {"data":{"name":"Asilbek"}}
+    return {"data":'blog list'}
 
-@app.get('/about')
-def about():
-    return {'data':["about page"]}
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'all unpublished data'}
+
+@app.get('/blog/{id}')
+def blog(id: int):
+    #fetch blog with id=id
+    return {'data': id}
+
+@app.get('/blog/{id}/comments')
+def comments(id: int):
+    #fetch comments of blog id=id
+    return {'data': {'1', '2'}}
+
