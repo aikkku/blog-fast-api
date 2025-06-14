d68fddd4560d3bd25dcc1081c4f182adcf5d6110
Asilbek Ismatilloev <asilbek@Asilbeks-MacBook-Air.local>
2025-06-14 16:46:14 -0400

    post

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
+from typing import Optional
+
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return {"data":'blog list'}
+@app.get('/blog')
+def index(limit:int = 10, published:bool = True, sort: Optional[str] = None):
+    # only get 10 published blogs
+    if published:
+        return {"data":f'{limit} published blog list'}
+    else:
+        return {"data":f'{limit} unpublished blog list'}
 
 @app.get('/blog/unpublished')
 def unpublished():
@@ -20,4 +27,16 @@ def blog(id: int):
 def comments(id: int):
     #fetch comments of blog id=id
     return {'data': {'1', '2'}}
+
+
+
+#POST
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool] = False
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return {'data':f'Blog is created with title: {blog.title}'}
 
