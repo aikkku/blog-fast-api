e65aee007361fdbf962e50eadb7226ae94ce7f87
Asilbek Ismatilloev <kodniy@gmail.com>
2025-06-19 17:15:58 -0400

    password hashing fix

--- blog/hashing.py
+++ blog/hashing.py
@@ -6,3 +6,4 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 class Hash():
     def bcrypt(password: str):
         hashedPassword = pwd_context.hash(password)
+        return hashedPassword
